702bd58cf6fb16818d463b7242b34fa399cad1b6
Máté Kiss <34075771+leopoldus97@users.noreply.github.com>
2021-10-10 17:06:23 +0200

    finally

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
 import "./App.css";
 import Temperature from "./pages/Temperature";
 // import NavbarComp from "./components/ui/NavbarComp";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  split,
+} from "@apollo/client";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:3000/graphql",
+});
+
+const wsLink = new WebSocketLink({
+  uri: "ws://localhost:3000/graphql",
+  options: {
+    reconnect: true,
+  },
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
 
 const client = new ApolloClient({
-  uri: "http://localhost:3000/graphql",
+  link: splitLink,
   cache: new InMemoryCache(),
 });
 
--- src/pages/Temperature.tsx
+++ src/pages/Temperature.tsx
@@ -1,10 +1,16 @@
-import { gql, useQuery } from "@apollo/client";
+import { gql, useQuery, useSubscription } from "@apollo/client";
 import { Line } from "react-chartjs-2";
 import { DataModel } from "../models/DataModel";
-import DateAdapter from '@mui/lab/AdapterDateFns';
+import DateAdapter from "@mui/lab/AdapterDateFns";
 import { DatePicker, LocalizationProvider } from "@mui/lab";
-import { FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
-import { useState } from "react";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
+import { useEffect, useState } from "react";
 
 type MeasurementType = "humidity" | "temperature";
 
@@ -28,45 +34,100 @@ const HUMIDITY_QUERY = gql`
   }
 `;
 
+const TEMPERATURE_SUBSCRIPTION = gql`
+  subscription TempSubscription {
+    record: temperatureAdded {
+      _id
+      measurementTime
+      value
+    }
+  }
+`;
+
+const HUMIDITY_SUBSCRIPTION = gql`
+  subscription HumSubscription {
+    record: humidityAdded {
+      _id
+      measurementTime
+      value
+    }
+  }
+`;
+
 const Temperature = () => {
   const [selectedDate, setSelectedDate] = useState(() => {
     const date = new Date();
     date.setHours(0, 0, 0, 0);
     return date;
   });
-  const [selectedMeasurement, setSelectedMeasurement] = useState<MeasurementType>("temperature");
+  const [selectedMeasurement, setSelectedMeasurement] =
+    useState<MeasurementType>("temperature");
+  const [dataArr, setDataArr] = useState<DataModel[]>([]);
 
-  const { loading, error, data } =
-    useQuery<{ records: DataModel[] }>(selectedMeasurement === "temperature" ? TEMPERATURES_QUERY : HUMIDITY_QUERY);
+  const { loading, error: queryError, data: queryData } = useQuery<{ records: DataModel[] }>(
+    selectedMeasurement === "temperature"
+      ? TEMPERATURES_QUERY
+      : HUMIDITY_QUERY
+  );
+
+  useEffect(() => {
+    if (queryData) {
+      setDataArr(prevArr => [...prevArr, ...queryData.records]);
+    }
+  }, [selectedMeasurement, queryData]);
+
+  const { error: subscriptionError, data: subscriptionData } = useSubscription<{ record: DataModel }>(
+    selectedMeasurement === "temperature"
+      ? TEMPERATURE_SUBSCRIPTION
+      : HUMIDITY_SUBSCRIPTION
+  );
+
+  useEffect(() => {
+    if (subscriptionData && subscriptionData.record) {
+      setDataArr((prevData) => [...prevData, subscriptionData.record]);
+    }
+  }, [subscriptionData]);
 
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
+  if (queryError || subscriptionError) return <p>Error: {queryError?.message || subscriptionError?.message}</p>;
 
   const chartData = (data: DataModel[]) => {
     return {
-      labels: data.map(record => new Date(record.measurementTime).toLocaleTimeString()),
+      labels: data.map((record) =>
+        new Date(record.measurementTime).toLocaleTimeString()
+      ),
       datasets: [
         {
-          label: selectedMeasurement === "temperature" ? "Temperature" : "Humidity",
-          data: data.map(temp => temp.value),
+          label:
+            selectedMeasurement === "temperature" ? "Temperature" : "Humidity",
+          data: data.map((temp) => temp.value),
           fill: false,
           backgroundColor: "rgb(255, 99, 132)",
           borderColor: "rgba(255, 99, 132, 0.2)",
-        }
+        },
       ],
     };
-  }
+  };
 
   const filterRecords = (record: DataModel) => {
     const date = new Date(record.measurementTime);
     const nextDay = new Date(selectedDate);
     nextDay.setDate(nextDay.getDate() + 1);
     return date >= selectedDate && date < nextDay;
-  }
+  };
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", width: "100%", height: "100%" }}>
-      <div style={{ margin: "16px 0px", display: "flex", flexDirection: "row" }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <div
+        style={{ margin: "16px 0px", display: "flex", flexDirection: "row" }}
+      >
         <LocalizationProvider dateAdapter={DateAdapter}>
           <DatePicker
             label="Basic example"
@@ -77,14 +138,28 @@ const Temperature = () => {
         </LocalizationProvider>
         <FormControl fullWidth>
           <InputLabel id="select-label">Measurement</InputLabel>
-          <Select label="Measurement" labelId="select-label" value={selectedMeasurement} onChange={ev => setSelectedMeasurement(ev.target.value as MeasurementType)}>
+          <Select
+            label="Measurement"
+            labelId="select-label"
+            value={selectedMeasurement}
+            onChange={(ev) =>
+              setSelectedMeasurement(ev.target.value as MeasurementType)
+            }
+          >
             <MenuItem value={"humidity"}>Humidity</MenuItem>
             <MenuItem value={"temperature"}>Temperature</MenuItem>
           </Select>
         </FormControl>
       </div>
       <div>
-        {data ? <Line style={{ flex: "0 0 20px" }} data={chartData(data.records.filter(filterRecords))} /> : <span>Loading...</span>}
+        {dataArr ? (
+          <Line
+            style={{ flex: "0 0 20px" }}
+            data={chartData(dataArr.filter(filterRecords))}
+          />
+        ) : (
+          <span>Loading...</span>
+        )}
       </div>
     </div>
   );
